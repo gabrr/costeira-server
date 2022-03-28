d8152254bfa27e0da74dbf5c734c832059fd24b5
gabrr <g.webdevelopr@gmail.com>
2022-03-27 21:37:09 -0300

    cors for client

--- src/setup/middlewaresSetup.ts
+++ src/setup/middlewaresSetup.ts
@@ -9,9 +9,14 @@ import cors from 'cors'
 import express from 'express'
 //import cookieParser from 'cookie-parser'
 
+const corsOptions = {
+  origin: CLIENT_URL,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 export default (server: Express) => {
     server.use(express.json())
-    server.use(cors({ origin: '*' }))
+    server.use(cors(corsOptions))
 		server.use('/static', express.static(path.join(__dirname, '.', '..', 'uploads')))
     //server.use(cookieParser())
 }
